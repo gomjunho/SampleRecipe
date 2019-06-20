c92b1179ce9dd9cfae9cee723c66717b112cb4db
junholee <gomjunho@gmail.com>
2019-06-21 08:50:27 +0900

    #12 #15 comment rest API, DB structure

--- api/routes/post.js
+++ api/routes/post.js
@@ -3,19 +3,42 @@
 
 // 에러 발생 원인 create, update, like 는 예약어라 컬럼명으로 사용 불가한듯
 CREATE TABLE post (
-id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-created datetime,
-updated datetime,
-user bigint(20),
-title varchar(32),
-description mediumtext,
-hit INT(11) unsigned NOT NULL default '0',
-recommend INT(11) unsigned NOT NULL default '0',
-PRIMARY KEY (id)
+    id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    created datetime,
+    updated datetime,
+    user bigint(20),
+    title varchar(32),
+    description mediumtext,
+    hit INT(11) unsigned NOT NULL default '0',
+    recommend INT(11) unsigned NOT NULL default '0',
+    PRIMARY KEY (id)
 );
 
 추가 고려사항: 사용자 삭제시 어떻게 처리할 것인가? 사용자가 작성한 글 삭제? 탈퇴한 사용자로 표기?(우선 탈퇴표기로 구현) foreign key 생성하지 않음
  */
+
+
+/*
+<comment data structure>
+
+CREATE TABLE comment (
+    id INT(11) unsigned NOT NULL AUTO_INCREMENT,
+    root INT(11),
+    re INT(1),
+    user BIGINT(11),
+    created DATETIME,
+    updated DATETIME,
+    comment mediumtext,
+    PRIMARY KEY (id)
+)
+
+// root: 원글 ID
+// re: 댓글 ID (없을 경우 depth 1/ 있을 경우 대댓글)
+
+
+// 조회시 댓글의 양이 많아질 경우 속도 저하의 원인이 될 수 있다. => 덧글 테이블 따로 분리하고, 원글 ID를 Index로 관리 하면 해결
+// DB에서 댓글을 링크드 리스트 형식으로 관리하면 좋을수 있지만 DB 접속을 여러번 해야하는 번거러움이 있다. => 원글 ID로 조회하여 한번에 글에 속한 댓글 조회 완료
+*/
 
 
 var express = require('express');
@@ -53,7 +76,8 @@ router.get('/', function (req, res) {
 
 router.get('/:id', function (req, res) {
     var id = req.params.id;
-    var sql = 'SELECT * FROM post WHERE id=?';
+    var sql = 'SELECT P.*, U.displayName FROM post P, users U WHERE P.user = U.id and P.id=?';
+
     conn.query(sql, [id], function(err, selectPostResults){
         if(err){
             console.log(err);
@@ -82,6 +106,70 @@ router.get('/:id', function (req, res) {
     });
 });
 
+router.get('/:id/comment', function (req, res) {
+    var id = req.params.id;
+
+    console.log('comment get',id);
+
+    var sql = 'SELECT C.id, C.root, C.re, U.displayName, C.created, C.updated, C.comment FROM comment C, users U WHERE C.user=U.id and C.root=?';
+    conn.query(sql, [id], function(err, selectPostResults){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(selectPostResults);
+        }
+    });
+});
+
+router.post('/:id/comment', function (req, res) {
+    var root = req.params.id;
+    var re = req.body.re;
+    var user = req.body.user.id;
+    var created = new Date();
+    var comment = req.body.comment;
+    
+    var sql = 'INSERT INTO comment (root, re, user, created, comment) VALUES (?, ?, ?, ?, ?)';
+    conn.query(sql, [root, re, user, created, comment], function(err, selectPostResults){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(selectPostResults);
+        }
+    });
+});
+
+router.put('/:Pid/comment/:Cid', function (req, res) {
+    var commentId = req.params.Cid;
+    var comment = req.body.comment;
+
+    var sql = 'UPDATE comment SET comment=? WHERE id=?';
+
+    conn.query(sql, [comment, commentId], function(err, updateCommentResults){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            console.log(updateCommentResults);
+            res.send(updateCommentResults);
+        }
+    });
+});
+
+router.delete('/:Pid/comment/:Cid', function (req, res) {
+    var commentId = req.params.Cid;
+    
+    var sql = 'DELETE FROM comment WHERE id=?';
+    conn.query(sql, [commentId], function(err, deletedCommentResults){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.send(deletedCommentResults);
+        }
+    });
+});
 
 router.put('/:id', function (req, res) {
     var id = req.params.id;
