0c3dcbb954b1cdc6b14c55ae98864fcb4f76f526
junholee <gomjunho@gmail.com>
2019-06-25 23:12:00 +0900

    Diary DB draft

--- front/src/components/Diary.vue
+++ front/src/components/Diary.vue
@@ -1,6 +1,7 @@
 <template>
     <div align="center" >
         <h1> Diary Application </h1>
+        UI에 집중 프로젝트 vue, CSS를 얼마나 잘 활용하는가?
 HTML 구조화 하고
 카드 형식으로 일기 표현
 카드 형식에는 달성도 % 표시
@@ -11,6 +12,10 @@ HTML 구조화 하고
 바디에는 카드형식의 일기.
 푸터에는 copyright.
 <br>
+Mongo DB
+로그인
+다이어리 (제목, 특이사항, 운동(사용자, 어깨, 이두, 삼두, 가슴, 하복근, 상복근, 다리, 가슴), 음식(사용자, 아침, 점심, 저녁, 사진), 개발(사용자, 프로젝트, issue, 해결))
+다이어리 CRUD rest api
         <hr>
     </div>
 </template>
